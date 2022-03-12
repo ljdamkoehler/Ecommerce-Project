65c5b67a7a1b1cd8db24ff81e6653f42a26dcaf3
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-03-12 09:38:36 -0600

    Require express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hi man!!')
+})
+
+app.listen(3000, ()=> {
+    console.log('App is listening on port 3000');
+})
