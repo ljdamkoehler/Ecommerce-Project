1e08061f1e485d90358077f5d337c522b632a347
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-03-13 12:56:26 -0500

    Setup EJS

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 
 app.get('/', (req, res) => {
     res.send('Hi man!!')
+})
+
+app.get('/admin/signup', (req, res) => {
+    res.render('adminSignup')
+})
+
+app.get('/admin/signin', (req, res) => {
+
 })
 
 app.listen(3000, ()=> {
