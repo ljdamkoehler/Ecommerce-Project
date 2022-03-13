5659a563f056cbc16e42d38a71b7b3b454f00520
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-03-13 13:29:53 -0500

    Parse form data manually

--- index.js
+++ index.js
@@ -7,11 +7,33 @@ app.set('views', path.join(__dirname, 'views'))
 
 
 app.get('/', (req, res) => {
-    res.send('Hi man!!')
+    res.send(`
+    <div>
+        <form method="POST">
+            <input type="text" name="email" id="" placeholder="email">
+            <input type="text" name="password" id="" placeholder="password">
+            <input type="text" name="passwordConfirmation" id="" placeholder="password confirmation">
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
+})
+
+app.post('/', (req, res) => {
+    req.on('data', data => {
+        const parsed = data.toString('utf8').split('&');
+        const formData = {};
+        for(let pair of parsed) {
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+        console.log(formData);
+    })
+    res.send('Posted babe!')
 })
 
 app.get('/admin/signup', (req, res) => {
-    res.render('adminSignup')
+    
 })
 
 app.get('/admin/signin', (req, res) => {
