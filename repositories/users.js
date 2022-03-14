97bc718a162d5a136f1ac37901301c428e2b8287
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-03-14 10:37:13 -0500

    Add password hashing and salting logic. Make sure it works with signup and signin forms

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
      constructor(filename) {
@@ -19,11 +22,27 @@ class UsersRepository {
      }
 
      async create(attrs) {
-         attrs.id = this.randomId();
+        attrs.id = this.randomId();
+
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buff = await scrypt(attrs.password, salt, 64)
+        
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${buff.toString('hex')}.${salt}`
+        };
+        records.push(record);
         await this.writeAll(records);
-        return attrs;
+        return record;
+     }
+
+     async comparePasswords(saved, supplied) {
+        const [hashed, salt] = saved.split('.');
+        const buffSupplied = await scrypt(supplied, salt, 64);
+
+        return hashed === buffSupplied.toString('hex');
+
      }
 
      async writeAll(records) {
