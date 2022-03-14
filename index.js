45a35529c748f5371b5b40f17377a49476800c33
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-03-13 21:57:57 -0500

    Setup cookies for authentication

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const app = express();
 const path = require('path');
 const usersRepo = require('./repositories/users');
@@ -8,10 +9,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: ['jpldze7815lxmsakja627g']
+}))
 
 app.get('/', (req, res) => {
     res.send(`
     <div>
+        Your id is: ${req.session.userId}
         <form method="POST">
             <input type="text" name="email" id="" placeholder="email">
             <input type="text" name="password" id="" placeholder="password">
@@ -33,6 +38,10 @@ app.post('/', async (req, res) => {
     if(password !== passwordConfirmation) {
         return res.send('Password and password confirmation do not match!')
     }
+    const { id } = await usersRepo.create({ email, password });
+
+    req.session.userId = id;
+
     res.send('Posted babe!');
 })
 
