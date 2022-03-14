12a2facc35d6be90ef72d31d57388a915727d9a9
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-03-13 20:18:42 -0500

    Create UserRepository Class for storing and accessing user data

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.send('Posted babe!')
+    res.send('Posted babe!');
 })
 
 app.get('/admin/signup', (req, res) => {
